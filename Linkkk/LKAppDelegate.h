b0041e9d62c2f3b1de38f06bec7cae9a29064b6e
Vincent Wen <wensi@live.com>
2013-04-15 08:21:42 -0400

    Initial Commit

---
+++ Linkkk/LKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LKAppDelegate.h
+//  Linkkk
+//
+//  Created by Vincent Wen on 4/15/13.
+//  Copyright (c) 2013 Linkkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Linkkk/LKViewController.h
@@ -0,0 +1,13 @@
+//
+//  LKViewController.h
+//  Linkkk
+//
+//  Created by Vincent Wen on 4/15/13.
+//  Copyright (c) 2013 Linkkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKViewController : UIViewController
+
+@end
